8c1587a41d3acc8ad96060d6fce16b3cc7e52813
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2023-11-08 10:41:45 +0400

    design updation

--- resources/views/frontend/common/client.blade.php
+++ resources/views/frontend/common/client.blade.php
@@ -13,7 +13,7 @@
                     <h2 class="mb-3 text-white text-start"> {{ $pageData->getTranslation('title') ?? '' }}</h2>
                     <div class="row g-1 align-items-center ">
                         @foreach($clients as $cli)
-                            <div class="col-partner col-sm-6 col-md-4  col-xl-2">
+                            <div class="col-partner col-6 col-sm-6 col-md-4  col-xl-2">
                                 <div class="partners-img-block">
                                     <img alt="" src="{{ asset($cli->getImage()) }}" class="img-fluid">
                                 </div>
