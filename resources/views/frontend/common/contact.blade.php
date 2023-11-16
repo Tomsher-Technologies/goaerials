eabe1966de24cea9dd81aa6af7c6c34dc6b9c959
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2023-11-16 16:14:01 +0400

    connect with us home page changes

--- resources/views/frontend/common/contact.blade.php
+++ resources/views/frontend/common/contact.blade.php
@@ -10,13 +10,14 @@
                     <div class="row">
                        
                         <h2 class="text-white mb-3">{{ $page->getTranslation('heading1') ?? '' }}</h2>
-                        <div class="col-md-6">
+                        
                             @php 
                                 $address12 = getFirstTwoAddress();
 
                             @endphp
                             @foreach($address12 as $onetwo)
-                            <div class="address-block">
+                            <div class="col-md-4">
+                                <div class="address-block">
                                     <h4>{{ $onetwo->getTranslation('place_name') }}</h4>
                                     <h5>{{ $onetwo->getTranslation('company_name') }}</h5>
                                     <p>{{ $onetwo->getTranslation('address') }}
@@ -24,25 +25,28 @@
                                     <a href="mailto:{{ $onetwo->email }}"> {{ $onetwo->email }}</a>
                                     <a href="tel:{{ $onetwo->phone }}"> {{ $onetwo->phone }}</a>
                                 </div>
+                            </div>
                             @endforeach
                            
-                        </div>
-                        <div class="col-md-6">
-                                @php 
-                                    $address34 = getThreeFourAddress();
-                                @endphp
-                                @foreach($address34 as $threefour)
-                                    <div class="address-block">
-                                        <h4>{{ $threefour->getTranslation('place_name') }}</h4>
-                                        <h5>{{ $threefour->getTranslation('company_name') }}</h5>
-                                        <p>{{ $threefour->getTranslation('address') }}
-                                        </p>
-                                        <a href="mailto:{{ $threefour->email }}"> {{ $threefour->email }}</a>
-                                        <a href="tel:{{ $threefour->phone }}"> {{ $threefour->phone }}</a>
-                                    </div>
-                                @endforeach
+                        
+                        
+                        @php 
+                            $address34 = getThreeFourAddress();
+                        @endphp
+                        @foreach($address34 as $threefour)
+                            <div class="col-md-4">
+                                <div class="address-block">
+                                    <h4>{{ $threefour->getTranslation('place_name') }}</h4>
+                                    <h5>{{ $threefour->getTranslation('company_name') }}</h5>
+                                    <p>{{ $threefour->getTranslation('address') }}
+                                    </p>
+                                    <a href="mailto:{{ $threefour->email }}"> {{ $threefour->email }}</a>
+                                    <a href="tel:{{ $threefour->phone }}"> {{ $threefour->phone }}</a>
+                                </div>
+                            </div>
+                        @endforeach
                             
-                        </div>
+                       
                     </div>
                     
                 </div>
