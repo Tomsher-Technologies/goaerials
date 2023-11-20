6313c17e1e3376e8f4848648ccf6dca90c34d221
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2023-11-20 17:14:32 +0400

    home contact page changes

--- resources/views/frontend/common/contact.blade.php
+++ resources/views/frontend/common/contact.blade.php
@@ -9,7 +9,7 @@
                 <div class="container">
                     <div class="row">
                        
-                        <h2 class="text-white mb-3">{{ $page->getTranslation('heading1') ?? '' }}</h2>
+                        <h2 class="text-white mb-3">{{ $page->getTranslation('heading2') ?? '' }}</h2>
                         
                             @php 
                                 $address12 = getFirstTwoAddress();
